a49a5c58f26c9402a34a5b6d253769d573ba0ebb
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-01-08 10:57:15 +0600

    Transfer to public

--- js/calculateTotal.js
+++ js/calculateTotal.js
@@ -1,11 +1,27 @@
+perPlayerExpence=0;
+managerCost=0;
+coachCost=0;
 
 document.getElementById('calculate-total').addEventListener('click', function(){
+    const perPlayerExpence= calculate;
+     const managerCost= parseFloat(getInputFieldById('manager-cost').value) ;
+     const coachCost = parseFloat(getInputFieldById('coach-cost').value) ;
 
+    console.log(managerCost ,  coachCost,  perPlayerExpence );
+    // calculate = calculate || 0;
+    // managerCost = managerCost || 0;
+    // coachCost= coachCost || 0;
+    // if ( managerCost === NaN  ){
+    //     perPlayerExpence= 0;
+    //     coachCost= 0;
+    //     managerCost= 0;
+    //     console.log(managerCost ,  coachCost,  perPlayerExpence );
+      
+    // }
 
-    perPlayerExpence= calculate;
-    managerCost= parseFloat(getInputFieldById('manager-cost').value) ;
-    coachCost = parseFloat(getInputFieldById('coach-cost').value) ;
-
+    // console.log(managerCost ,  coachCost, calculate );
+    
+    
     totalCost= perPlayerExpence + managerCost + coachCost;
 
     document.getElementById('total-cost').innerText=  totalCost;
